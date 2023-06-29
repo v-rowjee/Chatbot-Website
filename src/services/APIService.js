9b872b873bd289e80062a4296032f2027c9dc096
Ved Rowjee <84242585+v-rowjee@users.noreply.github.com>
2023-06-29 22:09:26 +0400

    .

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,33 +1,33 @@
 import { toast } from 'react-toastify';
 
-export default class APIService{
-    static sendRequest(body){
+export default class APIService {
+    static sendRequest(body) {
         // var host = process.env.SERVER_URL
         var host = "https://aac4-102-113-28-112.ngrok-free.app"
         return fetch(host + `/webhooks/rest/webhook`, {
             method: 'POST',
             mode: 'no-cors',
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS,GET,DELETE',
-                'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token',
+                'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS,GET,DELETE',
+                'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
             },
             body: JSON.stringify(body),
         })
-        .then(response => response.json())
-        .catch(error => {
-            // console.error('Error fetching data from server:', error)
-            toast.error('An error occurred while sending message. Please try again later.', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
+            .then(response => response.json())
+            .catch(error => {
+                // console.error('Error fetching data from server:', error)
+                toast.error('An error occurred while sending message. Please try again later.', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
             })
-        })
     }
 }
