a3b149095a68f4a482f191b33701bae7454541c4
Ved Rowjee <84242585+v-rowjee@users.noreply.github.com>
2023-07-26 16:48:42 +0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { ReactComponent as Wave3 } from '../assets/wave3.svg';
+import { Container } from 'react-bootstrap';
 
 export default function Footer(props) {
 
@@ -9,10 +8,9 @@ export default function Footer(props) {
         return (
             <>
                 <div className='sticky-top mt-5'>
-                    {/* <Wave3 /> */}
                     <footer className="bg-primary opacity-75 p-3">
                         <Container fluid className="pb-3">
-                            <p className='float-md-end text-light m-0'>© {new Date().getFullYear()} FYP. All Rights Reserved.</p>
+                            <p className='float-md-end text-light m-0'>© {new Date().getFullYear()} Cibo. All Rights Reserved.</p>
                         </Container>
                     </footer>
                 </div>
